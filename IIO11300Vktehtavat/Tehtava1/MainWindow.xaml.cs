58c6955c50e5e991d222747073c6fe0257ca1353
H8855 <madukthedoppelganger@gmail.com>
2016-01-13 10:34:18 +0200

    Modify MainWindow.

--- IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
 ﻿/*
 * Copyright (C) JAMK/IT/Esa Salmikangas
 * This file is part of the IIO11300 course project.
-* Created: 12.1.2015
-* Authors: Esa Salmikangas
+* Created: 12.1.2016 Modified: 13.1.2016
+* Authors: Kalle Rantala, Esa Salmikangas
 */using System;
 using System.Collections.Generic;
 using System.Linq;
