b1fbcb65fa40e5f40308452abc95113e4df990c7
H8855 <madukthedoppelganger@gmail.com>
2016-01-27 09:46:49 +0200

    Add file choosing.

--- IIO11300Vktehtavat/Harjoitus1MediaPlayer/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Harjoitus1MediaPlayer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,19 +24,21 @@ namespace Harjoitus1MediaPlayer
         public MainWindow()
         {
             InitializeComponent();
-            LoadMediaFile();
         }
         private void LoadMediaFile()
         {
             try
             {
-                //ladataan mediatiedosto
-                string filu = @"D:\H8855\CoffeeMaker.mp4";
+                //ladataan käyttäjän antama mediatiedosto
+                //string filu = @"D:\H8855\CoffeeMaker.mp4";
+                string filu = txtFileName.Text;
                 //tutkitaan onko tiedosto olemassa
                 if (System.IO.File.Exists(filu))
                 {
                     mediaElement.Source = new Uri(filu);
                 }
+                else
+                    MessageBox.Show("Tiedostoa " + filu + " ei löydy.");
             }
             catch (Exception ex)
             {
@@ -45,17 +48,49 @@ namespace Harjoitus1MediaPlayer
 
         private void btnPlay_Click(object sender, RoutedEventArgs e)
         {
-            mediaElement.Play();         
+            //LoadMediaFile();
+            mediaElement.Play();
+            //muutetaan nappuloiden tilaa
+            ChangeButtonsState();        
         }
 
         private void btnPause_Click(object sender, RoutedEventArgs e)
         {
             mediaElement.Pause();
+ 
         }
 
         private void btnStop_Click(object sender, RoutedEventArgs e)
         {
             mediaElement.Stop();
+            ChangeButtonsState();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            //avataan vakio Open-dialogi jotta käyttäjä voi valita yhden tiedoston
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.InitialDirectory = "d:\\H8855";
+            dlg.Filter = "Rock files (*.mp3)|*.mp3|Media files (.wmv)|.wmv|All files (*.*)|*.*";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                txtFileName.Text = dlg.FileName;
+                LoadMediaFile();
+            }
+        }
+
+        private void txtFileName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            LoadMediaFile();
+        }
+
+        private void ChangeButtonsState()
+        {
+            //muutetaan nappuloiden tilaa
+            btnPause.IsEnabled = !btnPause.IsEnabled;
+            btnStop.IsEnabled = !btnStop.IsEnabled;
+            btnPlay.IsEnabled = !btnPlay.IsEnabled;
         }
     }
 }
