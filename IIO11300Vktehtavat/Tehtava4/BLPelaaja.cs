4b39e3e5d5df2a12186e7cc41a826929ec7152e0
H8855 <madukthedoppelganger@gmail.com>
2016-02-10 01:18:17 +0200

    Tehtävä 4 kesken.

---
+++ IIO11300Vktehtavat/Tehtava4/BLPelaaja.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava4
+{
+    class Pelaaja
+    {
+        #region PROPERTIES
+
+        private string etunimi;
+
+        public string Etunimi
+        {
+            get { return etunimi; }
+            set { etunimi = value; }
+        }
+
+        private string sukunimi;
+
+        public string Sukunimi
+        {
+            get { return sukunimi; }
+            set { sukunimi = value; }
+        }
+
+        private string kokonimi;
+
+        public string Kokonimi
+        {
+            get { return kokonimi; }
+        }
+
+        private string esitysnimi;
+
+        public string Esitysnimi
+        {
+            get { return esitysnimi; }
+        }
+
+        private string seura;
+
+        public string Seura
+        {
+            get { return seura; }
+            set { seura = value; }
+        }
+
+        private int siirtohinta;
+
+        public int Siirtohinta
+        {
+            get { return siirtohinta; }
+            set { siirtohinta = value; }
+        }
+
+        public String DataMuoto
+        {
+            get { return etunimi + ";" + sukunimi + ";" + seura + ";" + siirtohinta; }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public Pelaaja(string etunimi, string sukunimi, string seura, int siirtohinta)
+        {
+            this.etunimi = etunimi;
+            this.sukunimi = sukunimi;
+            this.seura = seura;
+            this.siirtohinta = siirtohinta;
+        }
+
+        public Pelaaja()
+        {
+            this.etunimi = "Matti";
+            this.sukunimi = "Meikäläinen";
+            this.seura = "Tappara";
+            this.siirtohinta = 2500;
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return esitysnimi;
+        }
+
+        #region METHODS
+
+        public void UpdateNames()
+        {
+            kokonimi = etunimi + " " + sukunimi;
+            esitysnimi = kokonimi + ", " + seura;
+        }
+
+        public static void SaveDataToFile(List<Pelaaja> dataa, string filu)
+        {
+            //kirjoitetaan data tiedostoon, jos tiedosto on jo olemassa, liitetään se olemassaolevaan
+            try
+            {
+                //tutkitaan onko tiedosto olemassa
+                if (!System.IO.File.Exists(filu))
+                {
+                    //luodaan uusi
+                    using (StreamWriter sw = File.CreateText(filu))
+                    {
+                        //käydään kokoelma läpi
+                        foreach (var item in dataa)
+                        {
+                            sw.WriteLine(item.DataMuoto);
+                        }
+                    }
+                }
+                else
+                {
+                    //lisätään olemassaolevaan tiedostoon
+                    using (StreamWriter sw = File.AppendText(filu))
+                    {
+                        foreach (var item in dataa)
+                        {
+                            sw.WriteLine(item.DataMuoto);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ IIO11300Vktehtavat/Tehtava4/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtava4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Pelaaja> pelaajat;
+        public MainWindow()
+        {
+            InitializeComponent();
+            IniMyStuff();
+        }
+
+        private void IniMyStuff()
+        {
+            pelaajat = new List<Pelaaja>();
+            cmbSeura.Items.Add("Tappara");
+            cmbSeura.Items.Add("JYP");
+        }
+
+        private void ApplyChanges()
+        {
+            //päivitetään UI vastaamaan kokoelman tietoja
+            lbData.ItemsSource = null;
+            lbData.ItemsSource = pelaajat;             
+        }
+
+        private void btnLuoPelaaja_Click(object sender, RoutedEventArgs e)
+        {
+            // pelaajat.Contains(Pelaaja.)
+            if (txtEtunimi.Text.Trim().Length > 0 && txtSukunimi.Text.Trim().Length > 0 && txtSiirtohinta.Text.Trim().Length > 0 && cmbSeura.SelectedItem != null &&
+                !OnkoPelaajaa(txtEtunimi.Text + " " + txtSukunimi.Text))
+            {
+                Pelaaja player = new Pelaaja(txtEtunimi.Text, txtSukunimi.Text, cmbSeura.SelectedValue.ToString(), int.Parse(txtSiirtohinta.Text));
+                player.UpdateNames();
+                pelaajat.Add(player);
+                ApplyChanges();
+
+            }
+            else MessageBox.Show("Tarkista kentät!");
+        }
+
+        private bool OnkoPelaajaa(string haettava)
+        {
+            foreach (var item in pelaajat)
+            {
+                if (item.Kokonimi == haettava)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void btnTalletaPelaaja_Click(object sender, RoutedEventArgs e)
+        {
+            /* Ei toimi, poistamisessa ongelma.
+            try
+            {
+                Pelaaja player = new Pelaaja(txtEtunimi.Text, txtSukunimi.Text, cmbSeura.SelectedValue.ToString(), int.Parse(txtSiirtohinta.Text));
+                player.UpdateNames();
+
+                int index = lbData.SelectedIndex;
+                lbData.Items.RemoveAt(index);
+                lbData.Items.Insert(index, player);
+                lbData.SelectedIndex = index;
+
+                ApplyChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            */
+        }
+
+        private void lbData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Pelaaja player = (Pelaaja)lbData.SelectedItem;
+            txtEtunimi.Text = player.Etunimi;
+            txtSukunimi.Text = player.Sukunimi;
+            txtSiirtohinta.Text = player.Siirtohinta.ToString();
+            cmbSeura.SelectedItem = player.Seura;
+
+        }
+
+        private void btnPoistaPelaaja_Click(object sender, RoutedEventArgs e)
+        {
+            /* poistamisessa ongelma.
+            pelaajat.RemoveAt(lbData.SelectedIndex);
+            ApplyChanges();
+            */
+        }
+
+        private void btnKirjoitaPelaajat_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
