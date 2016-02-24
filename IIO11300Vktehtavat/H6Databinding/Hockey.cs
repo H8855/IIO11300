21068fe25246127054883b383f8320533ba575b5
H8855 <madukthedoppelganger@gmail.com>
2016-02-24 11:17:21 +0200

    Create H6Databinding.

---
+++ IIO11300Vktehtavat/H6Databinding/Hockey.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H6Databinding
+{
+    public class HockeyTeam
+    {
+        #region PROPERTIES
+        //huom public field ei kelpaa WPF:n databindingissa, pitää olla property
+        public string Name { get; set; }
+        public string City { get; set; }
+        #endregion
+        #region CONSTRUCTORS
+        public HockeyTeam()
+        {
+            Name = "";
+            City = "Unknown";
+        }
+        public HockeyTeam(string name, string city)
+        {
+            Name = name;
+            City = city;
+        }
+        #endregion
+        public override string ToString()
+        {
+            return Name + "@" + City;
+        }
+    }
+
+    public class HockeyLeague
+    {
+        //perustetaan SMLiiga, sisältää x kpl joukkueita
+        List<HockeyTeam> teams = new List<HockeyTeam>();
+        public HockeyLeague()
+        {
+            teams.Add(new HockeyTeam("HIFK", "Helsinki"));
+            teams.Add(new HockeyTeam("JYP", "Jyväskylä"));
+            teams.Add(new HockeyTeam("Kalpa", "Kuopio"));
+            teams.Add(new HockeyTeam("Sport", "Vaasa"));
+        }
+        //metodi joka palauttaa Liigan joukkueet
+        public List<HockeyTeam> GetTeams()
+        {
+            return teams;
+        }
+    }
+}
+
