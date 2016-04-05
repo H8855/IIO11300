97812771696dc7f8b26a3de61b4ee1f93badc01f
H8855 <madukthedoppelganger@gmail.com>
2016-04-05 13:17:37 +0300

    Tag tests.

---
+++ IIO11300Vktehtavat/TagTest/WinAPITest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TagTest
+{
+    class WinAPITest
+    {
+        private static string GetValue(IShellProperty value)
+        {
+            if (value == null || value.ValueAsObject == null)
+            {
+                return String.Empty;
+            }
+            return value.ValueAsObject.ToString();
+        }
+
+        public static void SetTags(string filename)
+        {
+            try
+            {
+                ShellObject picture = ShellObject.FromParsingName(filename);
+                var writer = picture.Properties.GetPropertyWriter();
+                writer.WriteProperty(SystemProperties.System.Keywords, "test 1; test 2");
+                writer.Close();          
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+                
+        }
+        public static void FormatJPEG(string filename)
+        {
+            System.Drawing.Image image1 = System.Drawing.Image.FromFile("@F:\\nwmain.bmp");
+            image1.Save(@"C:\testi.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/WinAPITester/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinAPITester
+{
+    class Program
+    {
+        private static string GetValue(IShellProperty value)
+        {
+            if (value == null || value.ValueAsObject == null)
+            {
+                return String.Empty;
+            }
+            return value.ValueAsObject.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+                return;
+
+            var filename = args[0];
+            if (!System.IO.File.Exists(filename))
+                return;
+
+            ShellFile picture;
+            picture = ShellFile.FromFilePath("F:\\testi\\nwmain.bmp"); 
+            //picture = ShellObject.FromParsingName(filename);
+
+            if (picture == null) return;
+
+            var writer = picture.Properties.GetPropertyWriter();
+            writer.WriteProperty(SystemProperties.System.Photo.CameraManufacturer, "test");
+            writer.WriteProperty(SystemProperties.System.Keywords, "Koira; test 2");
+            writer.Close();
+
+            var camera = GetValue(picture.Properties.GetProperty(SystemProperties.System.Photo.CameraManufacturer));
+            var cameraModel = GetValue(picture.Properties.GetProperty(SystemProperties.System.Photo.CameraModel));
+            var formattedString = String.Format("File {0} has Manufacturer {1} and Model {2}",
+                filename, camera, cameraModel);
+            Console.WriteLine(formattedString);
+            Console.ReadLine();
+        }
+    }
+}
